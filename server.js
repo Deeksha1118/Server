10313e1a7e7b2f2e35b52770508d58e897ead539
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-01-04 17:10:33 +0000

    demo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 8081;
 
 http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type" : "text/html"});
-    res.write("<h2>hey server started!!</h2>");
+    res.write("<h2>hey server got started!!</h2>");
     res.end();
 })
 .listen(port, () =>{
