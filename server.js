2aacf290096c7f55e7366257984ba1d9d85e97a5
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-01-07 10:18:15 +0000

    server

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const http = require("http");
 
 const port = 8081;
 
+const todoList = ["learn", "apply things", "succed"];
+
 http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type" : "text/html"});
-    res.write("<h2>hey server got started!!</h2>");
+    const {method, url} = req;
+    // console.log(method, url);
+    if(url == "/todos"){
+        if(method == "GET"){
+            res.writeHead(200, {"Content-Type" : "text/html"});
+            res.write(todoList.toString());
+        }
+    }
     res.end();
+    // res.writeHead(200, {"Content-Type" : "text/html"});
+    // res.write("<h2>hey server got started!!</h2>");
+    // res.end();
 })
 .listen(port, () =>{
     console.log(`NodeJS Server started running on post ${port}`);
