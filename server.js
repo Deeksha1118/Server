152404f790d3cdddc39954cfdee4d5c1326c9930
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-01-07 14:44:18 +0000

    express server

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
-const http = require("http");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
 
 const port = 8081;
 
 const todoList = ["learn", "apply things", "succed"];
 
-http.createServer((req, res) => {
-    const {method, url} = req;
-    // console.log(method, url);
-    if(url == "/todos"){
-        if(method == "GET"){
-            res.writeHead(200, {"Content-Type" : "text/html"});
-            res.write(todoList.toString());
-        }
-    }
-    res.end();
-    // res.writeHead(200, {"Content-Type" : "text/html"});
-    // res.write("<h2>hey server got started!!</h2>");
-    // res.end();
-})
-.listen(port, () =>{
-    console.log(`NodeJS Server started running on post ${port}`);
+app.get("/todos",(req,res) => {
+    res.status(200).send(todoList);
+});
+
+app.post("/todos",(req,res) => {
+    let newTodoList = req.body.name;
+    todoList.push(newTodoList);
+    res.status(201).send({message: "Task Added Successfully"});
 })
 
-// http://localhost:8081
+app.delete("/todos",(req,res) => {
+    const deleteThisItem = req.body.name;
+
+    todoList.find((ele, index) => {
+        if(ele === deleteThisItem) {
+            todoList.splice(index, 1);
+        }
+        res.status(202).send({message : `Deleted item ${req.body.name}`});
+    });
+});
+
+app.all("*",(req,res) => {
+    res.status(501).send();
+});
+
+app.listen(port, () => {
+    console.log(`NodeJS Server Started Running on Port ${port}`);
+});
